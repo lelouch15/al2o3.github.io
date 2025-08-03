f1e72cae29fe869630fecaaecf0561668764c051
lelouch15 <77967424+lelouch15@users.noreply.github.com>
2025-08-03 12:50:32 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig({ 
+   base: '/al2o3.github.io/',
+   plugins: [react()], 
+ })
